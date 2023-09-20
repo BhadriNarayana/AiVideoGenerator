1f864d934d556d8087df594f29f9eae218e41dab
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-09-20 15:47:15 +0530

    Finished the UI Structure

--- app.py
+++ app.py
@@ -1,17 +1,33 @@
 from kivy.app import App
-
+from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
+from kivy.lang import Builder
+import time
+
+from kivy.uix.videoplayer import VideoPlayer
+
+
+Builder.load_file('app.kv')
+
+class ScreenOne(Screen):
+    def generate(self):
+        self.manager.current = "second"
+
+class ScreenTwo(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        video_player = VideoPlayer(source="FinalOne.mp4")
+        self.add_widget(video_player)
 
 class AiVideoGenerator(App):
     def build(self):
-        
-        layout =  BoxLayout(orientation = 'vertical', spacing = 10, padding = 40)
-        textinput = TextInput(text='Hello world', size_hint=(1, 0.9))
-        btn1 = Button(text = "Generate Video", size_hint=(1, 0.1))
-        layout.add_widget(textinput)
-        layout.add_widget(btn1)
-        return layout
+        sm = ScreenManager()
+        one = ScreenOne(name = "first")
+        two = ScreenTwo(name = "second")
+        sm.add_widget(one)
+        sm.add_widget(two)
+        return sm
 
 AiVideoGenerator().run()    
